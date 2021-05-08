8df999a2501c24b556db9c967e7bbeb977bce6f6
Ievgen Yamamoto <ievgen@office.cquelle.de>
2021-05-08 22:01:56 +0200

    plug micro frontend into the company application

--- company/src/app/app-routing.module.ts
+++ company/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from './utils/federation-utils';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'calendar',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteName: 'calendar',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: 'CalendarModule',
+      }).then((m) => m.CalendarModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
